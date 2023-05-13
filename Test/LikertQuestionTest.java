227fac16c07dec91f4e63e0760c0a694b8ef1b3e
Christopher Daly <83555576+c-daly@users.noreply.github.com>
2023-05-13 16:52:37 -0400

    Delete Test directory
    mislaid commit

--- Test/LikertQuestionTest.java
+++
@@ -1,8 +0,0 @@
-public class LikertQuestionTest {
-  // just test that instantiation results
-  // in the correct question text and
-  // the correct 1-5 range
-  // test that sending an invalid answer results in incorrect
-  // everything else is correct
-
-}
--- Test/MultipleChoiceQuestionTest.java
+++
@@ -1,5 +0,0 @@
-public class MultipleChoiceQuestionTest {
-  // test instnatiation, options, answer, and question match args
-  // test sorting
-  // test trying to pass more than one answer
-}
