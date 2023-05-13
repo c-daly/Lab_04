005739577e5bc1ec3f7bf01821f2fcb4653a05d6
Christopher Daly <83555576+c-daly@users.noreply.github.com>
2023-05-13 16:51:23 -0400

    Delete src/questions directory
    mislaid commit

--- src/questions/AbstractQuestion.java
+++
@@ -1,9 +0,0 @@
-package questions;
-
-public abstract class AbstractQuestion implements Question {
-  protected String questionText;
-
-  public AbstractQuestion(String questionText) {
-    this.questionText = questionText;
-  }
-}
